6024371712ce7004a0fda68bfb953a1aa9dc1ffe
JEEJY30 <113361659+JEEJY30@users.noreply.github.com>
2023-01-05 15:20:52 +0400

    version 3

--- sanity.ts
+++ sanity.ts
@@ -3,8 +3,8 @@ import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  apiVersion: '2021-03-25',
+  projectId: 'j8g1o17j',
+  apiVersion: '2023-01-01',
   useCd: process.env.NODE_ENV === 'production'
 }
 
